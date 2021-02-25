24ccb75435945fe780ede92feb55b520c4b54710
Frederick Price <rprice@pricemail.ca>
2021-02-25 18:00:14 -0500

    Try just changing the keyboard

--- X11_stuff/xinitrc.d/60-Alter_CapsLock.sh
+++ X11_stuff/xinitrc.d/60-Alter_CapsLock.sh
@@ -1,2 +1,4 @@
 # maps caps lock to the escape key AND left control key (for Neovim and because it's so sweet).
-setxkbmap -option 'caps:ctrl_modifier';xcape -e 'Caps_Lock=Escape'&
+# setxkbmap -option 'caps:ctrl_modifier';xcape -e 'Caps_Lock=Escape'&
+# setxkbmap -model thinkpad -layout us -variant dvorak -option caps:ctrl_nocaps;xcape -e 'Caps_Lock=Escape'&
+setxkbmap -model thinkpad -layout us -variant dvorak -option ctr:nocaps
