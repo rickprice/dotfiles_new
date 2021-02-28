662a509cd0f60be985596b0b1ff1fff4caaebad4
Frederick Price <rprice@pricemail.ca>
2021-02-27 20:01:10 -0500

    Make a fix

--- setup_os.sh
+++ setup_os.sh
@@ -5,8 +5,10 @@ set -e
 
 ## keep track of the last executed command
 trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
-# echo an error message before exiting
-trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT Setup Packages for Arch
+## echo an error message before exiting
+trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT 
+
+#Setup Packages for Arch
 
 ## Upgrade everything
 sudo pacman --noconfirm -Syu
