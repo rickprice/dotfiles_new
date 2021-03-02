1812dca3ccac193aaca1f28397e0ed546d7e23e3
Frederick Price <rprice@pricemail.ca>
2021-03-02 15:00:47 -0500

    Get rid of trailing whitespace

--- setup_os.sh
+++ setup_os.sh
@@ -6,7 +6,7 @@ set -e
 ## keep track of the last executed command
 trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 ## echo an error message before exiting
-trap 'echo "\"${last_command}\" command failed with exit code $?."' EXIT 
+trap 'echo "\"${last_command}\" command failed with exit code $?."' EXIT
 
 #Setup Packages for Arch
 
