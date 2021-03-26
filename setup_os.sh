651f71347a7b2ea7dcd13d8c6012924d62ca8619
Frederick Price <rickp@activestate.com>
2021-03-26 13:41:39 -0400

    New AS requirements

--- setup_os.sh
+++ setup_os.sh
@@ -56,6 +56,7 @@ yay --noconfirm -S fwupd
 yay --noconfirm -S bazelisk-bin
 yay --noconfirm -S meld
 yay --noconfirm -S aws-cli
+yay --noconfirm -S postgresql-libs # Needed for ActiveState BE tools
 
 ## Required for initial_setup.py
 # PyYaml is pulled in automatically via udiskie and it conflicts with the Pip version
