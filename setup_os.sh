37bd97ab850906d4b2d35df559b7ed96b131c992
Frederick Price <rprice@pricemail.ca>
2021-03-31 15:42:31 -0400

    Setup Wezterm a little bit

--- setup_os.sh
+++ setup_os.sh
@@ -58,6 +58,7 @@ yay --noconfirm -S meld
 yay --noconfirm -S aws-cli
 yay --noconfirm -S postgresql-libs # Needed for ActiveState BE tools
 yay --noconfirm -S nerd-fonts-complete
+yay --noconfirm -S wezterm-git
 
 ## Required for initial_setup.py
 # PyYaml is pulled in automatically via udiskie and it conflicts with the Pip version
